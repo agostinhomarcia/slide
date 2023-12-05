c69801ecc8e556de491f29ccee60b963105eca94
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2023-12-05 20:00:59 -0300

    first commit

---
+++ src/db/imagens.js
@@ -0,0 +1,33 @@
+const arrayImagens = [
+  {
+    url: "https://img.freepik.com/fotos-gratis/mao-humana-digitando-no-teclado-do-computador-a-noite-gerada-por-ia_188544-31050.jpg?w=826&t=st=1701814344~exp=1701814944~hmac=5f93bdbf6f11a11dc13af308b854a34acc521dc610056c3b99439c2fae97a31c",
+    alt: 'Texto Alternativo',
+    link: '#'
+
+  },
+  {
+    url: "https://img.freepik.com/fotos-premium/vista-superior-do-computador-de-super-alto-desempenho-usado-para-videogames-na-sala-de-jogos_308072-2033.jpg?w=740",
+    alt: 'Texto Alternativo2',
+    link: '#'
+
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/colagem-do-conceito-saas_23-2149399294.jpg?w=740&t=st=1701814384~exp=1701814984~hmac=0b5550f03fe0d89eaa36a97aeeac411f5e368f06ca617c36af2274b2ed26f9cf",
+    alt: 'Texto Alternativo3',
+    link: '#'
+
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/uma-pessoa-digitando-no-laptop-a-noite-gerada-por-ia_188544-27872.jpg?w=826&t=st=1701814628~exp=1701815228~hmac=4a53dd01e7a1818cc7e10ab89263c1ac621e0a5cf6f2c65f2b59b8a628325cc3",
+    alt: 'Texto Alternativo4',
+    link: '#'
+
+  },
+  {
+    url: "https://img.freepik.com/fotos-gratis/complexidade-brilhante-de-chip-de-computador-da-industria-eletronica-gerada-por-ia_188544-36452.jpg?w=826&t=st=1701816346~exp=1701816946~hmac=e78dc8c311aa5e66a6d1762a1f17d3b8249f1dff1c19acde662c92f3b2aa8133",
+    alt: 'Texto Alternativo5',
+    link: '#'
+
+  },
+]
+export default arrayImagens
